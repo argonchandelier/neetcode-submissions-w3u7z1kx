5ca90a3d10925f2d47b526b559a0296b584365a6
Logan Richan <121440901+argonchandelier@users.noreply.github.com>
2026-04-09 01:32:06 -0700

    Add: rotting-fruit - submission-2

---
+++ Data Structures & Algorithms/rotting-fruit/submission-2.py
@@ -0,0 +1,43 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        rotten = []
+        nFresh = 0
+        seen = set()
+        for r, row in enumerate(grid):
+            for c, val in enumerate(row):
+                if val == 2:
+                    rotten.append((r,c))
+                    seen.add((r,c))
+                elif val == 1:
+                    nFresh += 1
+        
+        q = deque(rotten)
+        '''
+        def addToQ(coords):
+            r, c = coords
+            if 0 <= r < m and 0 <= c < n and coords not in seen and grid[r][c] == 1:
+                nonlocal nFresh
+                q.append(coords)
+                seen.add(coords)
+                nFresh -= 1
+        '''
+
+        mins = 0
+        while q:
+            mins += 1
+            for size in range(len(q)):
+                coords = q.popleft()
+                r, c = coords
+                for coords2 in [(r+1, c), (r, c+1), (r-1, c), (r, c-1)]:
+                    r2, c2 = coords2
+                    if 0 <= r2 < m and 0 <= c2 < n and coords2 not in seen and grid[r2][c2] == 1:
+                        q.append(coords2)
+                        seen.add(coords2)
+                        nFresh -= 1
+            if not q:
+                mins -= 1
+        
+        if nFresh > 0:
+            return -1
+        return mins
