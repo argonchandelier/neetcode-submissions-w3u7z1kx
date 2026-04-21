ca76d69e6f4592680b2126e6b9e8586c318fb558
Logan Richan <121440901+argonchandelier@users.noreply.github.com>
2026-04-20 21:05:21 -0700

    Add: jump-game-ii - submission-3

---
+++ Data Structures & Algorithms/jump-game-ii/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return 0
+
+        first, last = 1, nums[0]
+        for ans in range(1, 1000):
+            if last >= n-1:
+                return ans
+            nfirst = last+1
+            for ind in range(first, last+1):
+                num = nums[ind]
+                last = max(last, num + ind)
+            first = nfirst
+            
