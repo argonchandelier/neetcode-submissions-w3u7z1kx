18977b0192e8fe87e7cfee87d30ebe964e5cec9e
Logan Richan <121440901+argonchandelier@users.noreply.github.com>
2026-04-26 15:29:52 -0700

    Add: plus-one - submission-1

---
+++ Data Structures & Algorithms/plus-one/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        ans = []
+        c = 1
+        for i, d in enumerate(digits[::-1]):
+            c, d = divmod(d+c, 10)
+            ans.append(d)
+        if c == 1:
+            ans.append(1)
+        
+        ans.reverse()
+        return ans
